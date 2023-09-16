80571ee6cefb87a5b787063246c6b61a521bb698
marc-vdm <m.t.van.der.meide@cml.leidenuniv.nl>
2023-09-16 15:41:33 +0200

    Fix URL

--- ab_plugin_scenariolink/tables/models.py
+++ ab_plugin_scenariolink/tables/models.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from urllib.error import HTTPError
 from activity_browser.ui.tables.models import PandasModel
 
 class FoldsModel(PandasModel):
@@ -9,10 +10,13 @@ class FoldsModel(PandasModel):
     def sync(self):
 
         # url to fetch scenarios list form
-        url = "https://raw.githubusercontent.com/polca/ScenarioLink/main/ab_plugin_scenarioLink/scenarios%20list/list.csv"
+        url = "https://raw.githubusercontent.com/polca/ScenarioLink/main/ab_plugin_scenariolink/scenarios%20list/list.csv"
         # load pandas dataframe from url
         # and specify that the first row has headers
-        self._dataframe = pd.read_csv(url, sep=';', header=0)
+        try:
+            self._dataframe = pd.read_csv(url, sep=';', header=0)
+        except HTTPError as e:
+            print('++failed to import data', e)
 
 
         self.updated.emit()
