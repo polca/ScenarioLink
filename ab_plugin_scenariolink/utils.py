0947e77d6e0d595821df8cef2778ff4a3a5f38fe
romainsacchi <r_s@me.com>
2023-09-17 14:02:36 +0200

    Fixing files zipping.

--- ab_plugin_scenariolink/utils.py
+++ ab_plugin_scenariolink/utils.py
@@ -60,6 +60,23 @@ def download_files_from_zenodo(record_id: str) -> [Package, None]:
     # Zenodo API endpoint to fetch datapackages
     url = f"https://zenodo.org/api/records/{record_id}"
 
+    # Create a folder to save the downloaded files
+    folder_name = appdirs.user_cache_dir('ActivityBrowser', 'ActivityBrowser')
+    if not os.path.exists(folder_name):
+        os.makedirs(folder_name)
+
+    print("Cache folder: ", folder_name)
+
+    # Define the ZIP filename based on the Zenodo record ID
+    zip_filename = f"{record_id}.zip"
+
+    # Check if the file already exists; if so, return the Package
+    if os.path.exists(os.path.join(folder_name, zip_filename)):
+        print(f"File {zip_filename} already exists in cache.")
+        return Package(os.path.join(folder_name, zip_filename))
+
+    print(f"Fetching data from Zenodo: {url}")
+
     # Perform GET request to fetch the raw JSON content
     response = requests.get(url, timeout=10)
 
@@ -69,18 +86,6 @@ def download_files_from_zenodo(record_id: str) -> [Package, None]:
         return None
 
     json_data = response.json()
-
-    # Create a folder to save the downloaded files
-    folder_name = appdirs.user_cache_dir('ActivityBrowser', 'ActivityBrowser')
-    if not os.path.exists(folder_name):
-        os.makedirs(folder_name)
-
-    # Define the ZIP filename based on the Zenodo record ID
-    zip_filename = f"{record_id}.zip"
-
-    # Check if the file already exists; if so, return the Package
-    if os.path.exists(os.path.join(folder_name, zip_filename)):
-        return Package(os.path.join(folder_name, zip_filename))
 
     # Change cursor to indicate ongoing process
     QApplication.setOverrideCursor(Qt.WaitCursor)
@@ -93,23 +98,28 @@ def download_files_from_zenodo(record_id: str) -> [Package, None]:
             file_url = file_info['links']['self']
             response = requests.get(file_url, timeout=10)
 
-            # Create a temporary directory to store the downloaded ZIP files
-            with tempfile.TemporaryDirectory() as tmpdirname:
-                downloaded_zip_path = os.path.join(tmpdirname, "downloaded.zip")
+            # Create a separate temporary directory to store the downloaded ZIP files
+            with tempfile.TemporaryDirectory() as download_tmpdirname:
+                downloaded_zip_path = os.path.join(download_tmpdirname, "downloaded.zip")
 
                 # Save the downloaded ZIP file locally
                 with open(downloaded_zip_path, 'wb') as file:
                     file.write(response.content)
 
-                # Extract the ZIP file's contents
-                with zipfile.ZipFile(downloaded_zip_path, 'r') as downloaded_zip:
-                    downloaded_zip.extractall(tmpdirname)
+                # Create another temporary directory for the extracted files
+                with tempfile.TemporaryDirectory() as extract_tmpdirname:
+                    # Extract the ZIP file's contents
+                    with zipfile.ZipFile(downloaded_zip_path, 'r') as downloaded_zip:
+                        downloaded_zip.extractall(extract_tmpdirname)
 
-                # Add the extracted files to the final ZIP file
-                for root, _, files in os.walk(tmpdirname):
-                    for file in files:
-                        final_zip.write(os.path.join(root, file), file)
-
+                    # Add the extracted files to the final ZIP file
+                    for root, _, files in os.walk(extract_tmpdirname):
+                        for file in files:
+                            # Calculate the relative path
+                            relative_path = os.path.relpath(os.path.join(root, file), extract_tmpdirname)
+                            # Add the file to the ZIP archive with its relative path
+                            final_zip.write(os.path.join(root, file), relative_path)
+        print("Done.")
     # Restore the original cursor
     QApplication.restoreOverrideCursor()
 
