606444395dbb4ddbe22f475f2c7a06b59732825d
marc-vdm <m.t.van.der.meide@cml.leidenuniv.nl>
2023-09-17 14:30:02 +0200

    Add labels, fix closing dialog bug, implement hiding second table

--- ab_plugin_scenariolink/signals.py
+++ ab_plugin_scenariolink/signals.py
@@ -2,7 +2,7 @@ from PySide2.QtCore import QObject, Signal
 
 class Signals(QObject):
     get_datapackage_from_record = Signal(str)
-    record_ready = Signal()
+    record_ready = Signal(bool)
 
     generate_db = Signal(list, dict, bool)
     block_sdf = Signal(bool)
